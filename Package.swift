9a6ede38b5e3285143fabd11f8ed13dc3c91c573
Tanuj Ravi Rao <email@tansanrao.com>
2026-08-11 11:13:00 -0400

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,42 @@
+// swift-tools-version:6.3
+import PackageDescription
+
+let package = Package(
+    name: "NexusKb",
+    platforms: [
+       .macOS(.v13)
+    ],
+    dependencies: [
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.121.4"),
+        // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.101.0"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "NexusKb",
+            dependencies: [
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "NIOCore", package: "swift-nio"),
+                .product(name: "NIOPosix", package: "swift-nio"),
+            ],
+            swiftSettings: swiftSettings
+        ),
+        .testTarget(
+            name: "NexusKbTests",
+            dependencies: [
+                .target(name: "NexusKb"),
+                .product(name: "VaporTesting", package: "vapor"),
+            ],
+            swiftSettings: swiftSettings
+        )
+    ]
+)
+
+var swiftSettings: [SwiftSetting] { [
+    .enableUpcomingFeature("ExistentialAny"),
+    .enableUpcomingFeature("InternalImportsByDefault"),
+    .enableUpcomingFeature("MemberImportVisibility"),
+    .enableUpcomingFeature("InferIsolatedConformances"),
+    .enableUpcomingFeature("ImmutableWeakCaptures"),
+] }
